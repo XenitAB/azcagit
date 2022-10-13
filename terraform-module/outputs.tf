e672970efd5fa5d4f2f0b0a3b72e9ad7cbc92b9a
Simon Gottschlag <simon.gottschlag@xenit.se>
2022-10-13 20:51:11 +0200

    fix ref again

--- terraform-module/outputs.tf
+++ terraform-module/outputs.tf
@@ -1,4 +1,4 @@
 output "azcagit_trigger_group_object_id" {
   description = "The Object ID for the azcagit trigger permission"
-  value       = azuread_group_member.azcagit_trigger["current"].group_object_id
+  value       = azuread_group.azcagit_trigger.id
 }
