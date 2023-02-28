28cc2fbe924a0d819f8a95bd2887f206d39a1dee
Simon Gottschlag <simon.gottschlag@xenit.se>
2023-02-28 09:24:44 +0100

    Update config test

--- src/config/config_test.go
+++ src/config/config_test.go
@@ -1,12 +1,42 @@
 package config
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestNewConfig(t *testing.T) {
+	envVarsToClear := []string{
+		"RESOURCE_GROUP_NAME",
+		"ENVIRONMENT",
+		"AZURE_SUBSCRIPTION_ID",
+		"MANAGED_ENVIRONMENT_ID",
+		"KEY_VAULT_NAME",
+		"OWN_CONTAINER_APP_NAME",
+		"OWN_RESOURCE_GROUP_NAME",
+		"CONTAINER_REGISTRY_SERVER",
+		"CONTAINER_REGISTRY_USERNAME",
+		"CONTAINER_REGISTRY_PASSWORD",
+		"RECONCILE_INTERVAL",
+		"CHECKOUT_PATH",
+		"GIT_URL",
+		"GIT_BRANCH",
+		"GIT_YAML_ROOT",
+		"DAPR_APP_PORT",
+		"DAPR_PUBSUB_NAME",
+		"DAPR_TOPIC_NAME",
+		"NOTIFICATIONS_ENABLED",
+		"NOTIFICATION_GROUP",
+		"DEBUG",
+	}
+
+	for _, envVar := range envVarsToClear {
+		restore := testTempUnsetEnv(t, envVar)
+		defer restore()
+	}
+
 	args := []string{
 		"/foo/bar/bin",
 		"--resource-group-name",
@@ -52,22 +82,30 @@ func TestNewConfig(t *testing.T) {
 
 func TestRedactedConfig(t *testing.T) {
 	cfgWithUserAndPass := Config{
-		ContainerRegistryPassword: "secret",
-		GitUrl:                    "https://foo:bar@foobar.io/abc.git",
+		ContainerRegistryPassword: "secret",                            // secretlint-disable
+		GitUrl:                    "https://foo:bar@foobar.io/abc.git", // secretlint-disable
 	}
 	require.Equal(t, "redacted", cfgWithUserAndPass.Redacted().ContainerRegistryPassword)
-	require.Equal(t, "https://foo:redacted@foobar.io/abc.git", cfgWithUserAndPass.Redacted().GitUrl)
+	require.Equal(t, "https://foo:redacted@foobar.io/abc.git", cfgWithUserAndPass.Redacted().GitUrl) // secretlint-disable
 
 	cfg := Config{
 		ContainerRegistryPassword: "",
-		GitUrl:                    "https://foobar.io/abc.git",
+		GitUrl:                    "https://foobar.io/abc.git", // secretlint-disable
 	}
 	require.Equal(t, "", cfg.Redacted().ContainerRegistryPassword)
-	require.Equal(t, "https://foobar.io/abc.git", cfg.Redacted().GitUrl)
+	require.Equal(t, "https://foobar.io/abc.git", cfg.Redacted().GitUrl) // secretlint-disable
 }
 
 func TestRedactUrl(t *testing.T) {
 	require.Equal(t, "https://foobar.io/abc.git", redactUrl("https://foobar.io/abc.git"))
-	require.Equal(t, "https://foo:redacted@foobar.io/abc.git", redactUrl("https://foo:bar@foobar.io/abc.git"))
+	require.Equal(t, "https://foo:redacted@foobar.io/abc.git", redactUrl("https://foo:bar@foobar.io/abc.git")) // secretlint-disable
 	require.Equal(t, "https://redacted@foobar.io/abc.git", redactUrl("https://foo@foobar.io/abc.git"))
+}
+
+func testTempUnsetEnv(t *testing.T, key string) func() {
+	t.Helper()
+
+	oldEnv := os.Getenv(key)
+	os.Unsetenv(key)
+	return func() { os.Setenv(key, oldEnv) }
 }
